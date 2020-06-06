b437249002221ebfb9da4404226265ac380822cc
Asım MISIRLI <asimmisirli@hotmail.com>
2020-06-06 21:55:38 +0300

    Update Zabbix5Install.sh

--- Zabbix5Install.sh
+++ Zabbix5Install.sh
@@ -1,1 +1,5 @@
-
+#Ubuntu20 server üzerine Zabbix 5 kurulumu
+sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+sudo dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+sudo apt update
+sudo apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
