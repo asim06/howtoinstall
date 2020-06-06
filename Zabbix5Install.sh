ec2f29837754b8621dbe42cd9da3c4d416766d10
Asım MISIRLI <asimmisirli@hotmail.com>
2020-06-06 22:50:54 +0300

    Update Zabbix5Install.sh

--- Zabbix5Install.sh
+++ Zabbix5Install.sh
@@ -1,14 +1,14 @@
 #Ubuntu20 server üzerine Zabbix 5 kurulumu
-sudo wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
-sudo dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
-sudo apt update
-sudo apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+wget https://repo.zabbix.com/zabbix/5.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+dpkg -i zabbix-release_5.0-1+$(lsb_release -sc)_all.deb
+apt update
+apt -y install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
 
 
 #Bu yapılandırma örneğinde Veritabanı ve Zabbix için strongPassword şifresini kullanıyorum.
 
 #Veritabanı Yapılandırması
-sudo mysql_secure_installation
+mysql_secure_installation
 
 #Aşağıda set root password Y seçeneğinden sonra strongPassword ifadesi girilerek veritabanı root kullanıcısının şifresi yapılandırılır.
 Enter current password for root (enter for none): Press the Enter
@@ -23,31 +23,29 @@ Reload privilege tables now? [Y/n]:  Y
 #Veritabanı üzerinde database oluşturma
 #Veritabanı ismi zabbix ve şifresi zabbixDBpass
 
-sudo mysql -uroot -p'strongPassword' -e "create database zabbix character set utf8 collate utf8_bin;"
-sudo mysql -uroot -p'strongPassword' -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDBpass';"
+mysql -uroot -p'strongPassword' -e "create database zabbix character set utf8 collate utf8_bin;"
+mysql -uroot -p'strongPassword' -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDBpass';"
 
-sudo mysql -uroot -p'strongPassword' zabbix -e "set global innodb_strict_mode='OFF';"
+mysql -uroot -p'strongPassword' zabbix -e "set global innodb_strict_mode='OFF';"
 zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -p'zabbixDBpass' zabbix
-sudo mysql -uroot -p'strongPassword' zabbix -e "set global innodb_strict_mode='ON';"
+mysql -uroot -p'strongPassword' zabbix -e "set global innodb_strict_mode='ON';"
 
-Zabbix_server.conf dosyası nano ile açılır herhangi bir alana DBPassword=zabbixDBpass satırı eklenir.
-sudo nano /etc/zabbix/zabbix_server.conf
+#Zabbix_server.conf dosyası nano ile açılır herhangi bir alana DBPassword=zabbixDBpass satırı eklenir.
+nano /etc/zabbix/zabbix_server.conf
 
-Zabbix Server ve Ajanı başlatılır
-sudo systemctl restart zabbix-server zabbix-agent 
-sudo systemctl enable zabbix-server zabbix-agent
+#Zabbix Server ve Ajanı başlatılır
+systemctl restart zabbix-server zabbix-agent 
+systemctl enable zabbix-server zabbix-agent
 
 Zabbix php configure dosyası açılır aşağıdaki yorum satırı aktif hale getirilir.Aşağıdaki şekilde yazılarak kayıt edilir.
-sudo nano /etc/zabbix/apache.conf
+nano /etc/zabbix/apache.conf
 “# php_value date.timezone Europe/Riga”
 php_value date.timezone Europe/Istanbul
 
 Web Server Yeniden başlatma
-sudo systemctl restart apache2
-sudo systemctl enable apache2
+systemctl restart apache2
+systemctl enable apache2
 
 http://server_ip/zabbix  adresi ile bağlantı sağlanır. Zabbix şifreniz zabbixDBpass 
 
-İşlem sonrası Admin/Zabbix ile giriş yapılır.
-
-
+#İşlem sonrası Admin/Zabbix ile giriş yapılır.
